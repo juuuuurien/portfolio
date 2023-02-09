1ab5a400e15359ac2274ae120e944fdf3779eeaf
Julien Lopez <julien@2leaf.com>
2023-02-09 11:57:59 -0800

    Updated portfolio

--- components/Content/About/About.js
+++ components/Content/About/About.js
@@ -2,15 +2,14 @@ import React from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import ModSquad from "../../../public/assets/modsquad.svg";
+import TwoLeaf from "../../../public/assets/2leaf.svg";
+import CFX from "../../../public/assets/CFX_white.png";
 import Selfie from "../../../public/assets/selfie.png";
 import Selfie2 from "../../../public/assets/selfie2.png";
+import Selfie3 from "../../../public/assets/selfie3.png";
 
-import {
-  AiFillGithub,
-  AiFillInstagram,
-  AiFillTwitterCircle,
-  AiFillLinkedin,
-} from "react-icons/ai";
+import { AiFillGithub, AiFillInstagram, AiFillTwitterCircle, AiFillLinkedin } from "react-icons/ai";
+import WorkCard from "./components/WorkCard";
 
 const About = ({ setNavHighlight }) => {
   const children = {
@@ -59,25 +58,21 @@ const About = ({ setNavHighlight }) => {
       initial="hidden"
       whileInView={"shown"}
       exit="exit"
-      className="flex flex-col mx-auto py-[5vh] lg:max-w-[55%] max-w-[80%] gap-6"
+      className="flex flex-col mx-auto py-[5vh] lg:max-w-[55%] max-w-[80%] gap-10"
       viewport={{ once: true, amount: 0.2 }}
     >
       <motion.h3 variants={children} className="section-heading mb-10">
         Who I am
       </motion.h3>
-      <div className="flex flex-col md:flex-row">
-        <div className=" my-10 md:mx-5">
+      <div className="flex flex-col md:flex-row max-w-[80%] self-center">
+        <div className="flex flex-col my-10 md:mx-5 justify-between">
           <motion.p variants={children}>
-            I am a <strong className="text-yellow-300">Fi</strong>
-            <strong className=" text-blue-500">li</strong>
-            <strong className="text-red-500">pi</strong>
-            <strong className=" text-white">no</strong>-American software
-            engineer specializing in the front end. Using modern technologies
-            such as React and React Native, I strive to make smooth,
+            I am a Filipino-American 🇵🇭 Software Engineer specializing in the Front End. Using
+            modern technologies such as React and React Native, I strive to make smooth,
             design-minded digital experiences!
           </motion.p>
-          <motion.p variants={children}>
-            I am currently working as a digital content specialist over at{" "}
+          {/* <motion.p variants={children}>
+            I am currently working as the Lead Developer over at 2Leaf Web Development{" "}
             <motion.span
               className="inline-block"
               whileHover={{
@@ -85,19 +80,28 @@ const About = ({ setNavHighlight }) => {
                 transition: { duration: 0.1 },
               }}
             >
-              <a href="https://modsquad.com/" target="_new">
-                <Image src={ModSquad} width="120" height="40" />
+              <a href="https://2leaf.com/" target="_new">
+                <Image src={TwoLeaf} width="40" height="40" />
               </a>
             </motion.span>
             {
-              ". At ModSquad, I work with a team of like-minded moderators to help manage, build, and protect online communities all over the world. "
+              "."
             }
           </motion.p>
-          <motion.p variants={children} className="text-[#CA9F0D]">
-            Although my current occupation is not 100% programming, it is my
-            dream to be a developer who is passionate about tech, web design,
-            and creating new things .
-          </motion.p>
+          <motion.p variants={children}>
+            I am currently in the middle of transitioning to a Front End position at the California Fruit Exchange.
+            <motion.span
+              className="inline-block"
+              whileHover={{
+                y: -5,
+                transition: { duration: 0.1 },
+              }}
+            >
+              <a href="https://2leaf.com/" target="_new">
+                <Image src={CFX} width="100" height="64" />
+              </a>
+            </motion.span> 
+          </motion.p> */}
           <motion.p variants={children}>
             Other than coding, I like to travel, paint, cook, and eat!
           </motion.p>
@@ -108,12 +112,89 @@ const About = ({ setNavHighlight }) => {
           className="flex self-center rounded-[50px] border-2 border-[#498fd5]"
         >
           <Image
-            src={Selfie2}
-            width={800}
-            height={800}
+            src={Selfie3}
+            width={640}
+            height={640}
             className="rounded-[50px] mix-blend-screen opacity-70 hover:opacity-100 duration-300 transition-all"
           />
         </motion.div>
+      </div>
+      <div className="flex flex-row gap-4 w-full justify-center mt-10">
+        <WorkCard>
+          <div className="flex flex-row justify-between align-center">
+            <motion.span
+              className="inline-block"
+              whileHover={{
+                y: -5,
+                transition: { duration: 0.1 },
+              }}
+            >
+              <a href="https://modsquad.com/" target="_new">
+                <Image src={ModSquad} width="64" height="50" />
+              </a>
+            </motion.span>
+            <p className="text-sm text-right">2020 - 2022</p>
+          </div>
+          <br />
+          <h5>ModSquad</h5>
+          <br />
+          <p className="text-sm mb-6">
+            At ModSquad, I worked with a team of like-minded moderators to help manage, build, and
+            protect online communities all over the world.
+          </p>
+          <ul className="text-left">
+            <li className="text-sm pl-5">
+              Participated in company wide content moderation proposals.
+            </li>
+            <li className="text-sm pl-5">
+              Was responsible for the onboarding of incoming content moderators.
+            </li>
+            <li className="text-sm pl-5">
+              Regularly adhered to and proposed moderation guidelines for the company and their
+              clients.
+            </li>
+          </ul>
+        </WorkCard>
+        <WorkCard>
+          <div className="flex flex-row justify-between align-center">
+            <motion.span
+              className="inline-block"
+              whileHover={{
+                y: -5,
+                transition: { duration: 0.1 },
+              }}
+            >
+              <a href="https://2Leaf.com/" target="_new">
+                <Image src={TwoLeaf} width="40" height="40" />
+              </a>
+            </motion.span>
+            <p className="text-sm text-right">2022 - 2023</p>
+          </div>
+          <br />
+          <h5>2Leaf Web Development</h5>
+          <br />
+          <p className="text-sm mb-6">
+            At 2Leaf Web Development, I was responsible for creating, maintaining, reviewing, and
+            shipping software created by for various clients around Sacramento and beyond.
+          </p>
+          <ul className="text-left">
+            <li className="text-sm pl-5">Built maintainable RESTful APIs from the ground up.</li>
+            <li className="text-sm pl-5">
+              Created beautiful and responsive websites and apps in JavaScript and TypeScript using
+              technologies such as React, React Native, plain HTML & CSS, PHP and more.
+            </li>
+            <li className="text-sm pl-5">
+              Built backend applications using technologies such as Node.js, Express, MongoDB ,
+              MySQL.
+            </li>
+            <li className="text-sm pl-5">
+              Deployed apps onto SaaS based platforms such as AWS EC2, AWS S3, cPanel and more.
+            </li>
+            <li className="text-sm pl-5">
+              Reviewed, merging, and shipping code created by my peers.
+            </li>
+          </ul>
+        </WorkCard>
       </div>
       <motion.div
         className="flex flex-col justify-center items-center mt-20 gap-10"
---
+++ components/Content/About/components/WorkCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WorkCard = ({ children }) => {
+  return (
+    <div className="flex flex-1 text-center flex-col gap-2 rounded-lg border-[#498fd5] border-2 max-w-[33%] px-7 py-6 bg-[#498fd522] hover:bg-transparent transition-all ">
+      {children}
+    </div>
+  );
+};
+
+export default WorkCard;
